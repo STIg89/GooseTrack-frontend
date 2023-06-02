05cb5611bc3d179098ff194346b7323fb969550a
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2023-06-02 17:19:32 +0300

    registration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
+import { useEffect, lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { setAuthHeader } from '../redux/auth/operations';
+import { refreshUser } from '../redux/auth/operations';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectToken } from '../redux/auth/selectors';
+
+const Register = lazy(() => import('../pages/Register/RegisterPage'));
+
+const App = () => {
+  const token = useSelector(selectToken);
+  setAuthHeader(token);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [token, dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Suspense fallback={''}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 };
+
+export default App;
---
+++ src/redux/auth/operations.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { Notify } from 'notiflix';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+export const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, thunkAPI) => {
+    try {
+      const { data } = await axios.post('/auth/register', user);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      Notify.failure('Please check your email and password and try again');
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const refreshUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue('Unable to fetch user');
+    }
+
+    try {
+      setAuthHeader(persistedToken);
+      const res = await axios.get('/users/current');
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
