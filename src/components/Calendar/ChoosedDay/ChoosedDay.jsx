b6fea3d76bcaf9de0c949d321bcdedf3eee84e0b
atevsl <3380033@ukr.net>
2023-06-03 19:18:23 +0300

    add status filter

--- src/components/Calendar/ChoosedDay/ChoosedDay.jsx
+++ src/components/Calendar/ChoosedDay/ChoosedDay.jsx
@@ -1,4 +1,5 @@
 import Container from './ChoosedDay.Styled';
+import DayCalendarHead from './DayCalendarHead/DayCalendarHead';
 import TasksColumnsList from './TasksColumnsList/TasksColumnsList';
 
 const ChoosedDay = () => {
@@ -7,20 +8,42 @@ const ChoosedDay = () => {
       taskText:
         'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, libero.',
       priority: 'low',
+      status: 'To do',
     },
     {
       taskText:
         'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, libero.',
       priority: 'medium',
+      status: 'In progress',
+    },
+    {
+      taskText:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, libero.',
+      priority: 'low',
+      status: 'To do',
     },
     {
       taskText:
         'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, libero.',
       priority: 'high',
+      status: 'Done',
+    },
+    {
+      taskText:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, libero.',
+      priority: 'high',
+      status: 'To do',
+    },
+    {
+      taskText:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, libero.',
+      priority: 'high',
+      status: 'In progress',
     },
   ];
   return (
     <Container>
+      <DayCalendarHead />
       <TasksColumnsList tasks={tasks} />
     </Container>
   );
--- src/components/Calendar/ChoosedDay/TasksColumnsList/TasksColumnsList.jsx
+++ src/components/Calendar/ChoosedDay/TasksColumnsList/TasksColumnsList.jsx
@@ -4,20 +4,24 @@ import TasksColumn from './TasksColumn/TasksColumn';
 import { ColumnsItem, ColumnsList } from './TasksColumnsList.Styled';
 
 const TasksColumnsList = ({ tasks }) => {
+  const todotasks = tasks.filter(item => item.status === 'To do');
+  const inprogresstasks = tasks.filter(item => item.status === 'In progress');
+  const donetasks = tasks.filter(item => item.status === 'Done');
+
   return (
     <ColumnsList>
       <ColumnsItem>
-        <TasksColumn headName="To do" tasks={tasks} />
+        <TasksColumn headName="To do" tasks={todotasks} />
         <AddTaskBtn />
       </ColumnsItem>
 
       <ColumnsItem>
-        <TasksColumn headName="In progress" tasks={tasks} />
+        <TasksColumn headName="In progress" tasks={inprogresstasks} />
         <AddTaskBtn />
       </ColumnsItem>
 
       <ColumnsItem>
-        <TasksColumn headName="Done" tasks={tasks} />
+        <TasksColumn headName="Done" tasks={donetasks} />
         <AddTaskBtn />
       </ColumnsItem>
     </ColumnsList>
