6a46ad6f03ecfc98479a71e6ad0639e2e8e2a3fb
STIg89 <andreystig89@gmail.com>
2023-06-05 02:25:42 +0300

    fix

--- src/components/MainLayout/SideBar/SideBar.jsx
+++ src/components/MainLayout/SideBar/SideBar.jsx
@@ -1,52 +1,59 @@
-import { useState } from "react"
-import { LuCalendarCheck2 } from "react-icons/lu"
-import { NavLink } from "react-router-dom"
-import LogOutBtn from "../SideBar/LogoutBtn/LogoutBtn"
-import { CgClose } from "react-icons/cg"
-import LogoImg from "../Header/test_image/GOOSE_LOGO.svg"
-import Logo_text from "../Header/test_image/GooseTrack.svg"
-import UserCheckIcon from "../Header/test_image/user-check-01.svg"
-import { CloseBtn, SideBarDiv, SideBarContainer, LogoDiv, SideBarNav, SideBarLinks, SideBarLi, Logo, GooseLogo, UserIcon, StyledNavLink, StyledCalendarIcon, TitleSideBar, GooseLogoText } from "./SideBar.styled"
+import LogOutBtn from '../SideBar/LogoutBtn/LogoutBtn';
+import { CgClose } from 'react-icons/cg';
+import LogoImg from '../Header/test_image/GOOSE_LOGO.svg';
+import UserCheckIcon from '../Header/test_image/user-check-01.svg';
+import {
+  CloseBtn,
+  SideBarDiv,
+  SideBarContainer,
+  LogoDiv,
+  SideBarNav,
+  SideBarLinks,
+  SideBarLi,
+  Logo,
+  GooseLogo,
+  UserIcon,
+  StyledNavLink,
+  StyledCalendarIcon,
+  TitleSideBar,
+} from './SideBar.styled';
 
 const Sidebar = () => {
+  return (
+    <SideBarContainer>
+      <SideBarDiv>
+        <LogoDiv>
+          <Logo>
+            <GooseLogo src={LogoImg} alt="" />
+            {/* <GooseLogoText src={Logo_text} alt="" /> */}
+          </Logo>
 
-    return (
-        <SideBarContainer>
-            <SideBarDiv>
-                <LogoDiv>
-                    <Logo>
-                        <GooseLogo src={LogoImg} alt="" />
-                        {/* <GooseLogoText src={Logo_text} alt="" /> */}
-                    </Logo>
+          <CloseBtn>
+            <CgClose />
+          </CloseBtn>
+        </LogoDiv>
+        <SideBarNav>
+          <TitleSideBar>User Panel</TitleSideBar>
+          <SideBarLinks>
+            <SideBarLi>
+              <StyledNavLink>
+                <UserIcon src={UserCheckIcon} alt="icon" />
+                My account
+              </StyledNavLink>
+            </SideBarLi>
 
-                    <CloseBtn>
-                        <CgClose />
-                    </CloseBtn>
+            <SideBarLi>
+              <StyledNavLink>
+                <StyledCalendarIcon />
+                Calendar
+              </StyledNavLink>
+            </SideBarLi>
+          </SideBarLinks>
+        </SideBarNav>
+      </SideBarDiv>
+      <LogOutBtn />
+    </SideBarContainer>
+  );
+};
 
-                </LogoDiv>
-                <SideBarNav>
-                    <TitleSideBar>User Panel</TitleSideBar>
-                    <SideBarLinks>
-                        <SideBarLi>
-                            <StyledNavLink>
-                                <UserIcon src={UserCheckIcon} alt="icon" />
-                                My account
-                            </StyledNavLink>
-                        </SideBarLi>
-
-                        <SideBarLi>
-                            <StyledNavLink>
-                                <StyledCalendarIcon />
-                                Calendar
-                            </StyledNavLink>
-                        </SideBarLi>
-                    </SideBarLinks>
-
-                </SideBarNav>
-            </SideBarDiv>
-            <LogOutBtn />
-        </SideBarContainer >
-    )
-}
-
-export default Sidebar
+export default Sidebar;
