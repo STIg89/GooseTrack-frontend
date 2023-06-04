998cfaa569f44f1a78a5f894e7a5f9396c6ac180
STIg89 <andreystig89@gmail.com>
2023-06-05 02:45:41 +0300

    Update LogoutBtn.jsx

--- src/components/MainLayout/SideBar/LogoutBtn/LogoutBtn.jsx
+++ src/components/MainLayout/SideBar/LogoutBtn/LogoutBtn.jsx
@@ -1,11 +1,16 @@
-import { FiLogOut } from "react-icons/fi"
-import { StyledLogOutBtn } from "./LogoutBtn.styled";
+import { FiLogOut } from 'react-icons/fi';
+import { StyledLogOutBtn } from './LogoutBtn.styled';
+import { logout } from 'redux/auth/operations';
+import { useDispatch } from 'react-redux';
 
 const LogOutBtn = () => {
+  const dispatch = useDispatch();
 
-    return (
-        <StyledLogOutBtn>Log out <FiLogOut /> </StyledLogOutBtn>
-    )
-}
+  return (
+    <StyledLogOutBtn type="button" onClick={() => dispatch(logout())}>
+      Log out <FiLogOut />{' '}
+    </StyledLogOutBtn>
+  );
+};
 
 export default LogOutBtn;
