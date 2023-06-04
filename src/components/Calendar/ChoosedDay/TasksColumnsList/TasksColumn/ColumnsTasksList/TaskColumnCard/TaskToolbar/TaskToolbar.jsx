f021ecdae4fae346ca3df62fe51089e9738591e7
atevsl <3380033@ukr.net>
2023-06-05 00:24:24 +0300

    add styles to categories window

--- src/components/Calendar/ChoosedDay/TasksColumnsList/TasksColumn/ColumnsTasksList/TaskColumnCard/TaskToolbar/TaskToolbar.jsx
+++ src/components/Calendar/ChoosedDay/TasksColumnsList/TasksColumn/ColumnsTasksList/TaskColumnCard/TaskToolbar/TaskToolbar.jsx
@@ -1,8 +1,17 @@
 import Icons from 'images/sprite.svg';
-import { BtnStyled, ToolBarItem, Wraper } from './TaskToolbar.Styled';
+import {
+  BtnArrow,
+  BtnStyled,
+  ChouseCat,
+  InputStyled,
+  LabelStyled,
+  ToolBarItem,
+  Wraper,
+} from './TaskToolbar.Styled';
 import { deleteTask, patchTask } from 'redux/tasks/operations';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectTasks } from 'redux/tasks/selectors';
+import { useState } from 'react';
 
 const TaskToolBar = ({ id }) => {
   const dispatch = useDispatch();
@@ -10,64 +19,58 @@ const TaskToolBar = ({ id }) => {
   let editTask = tasks.find(task => task._id === id);
 
   const category = ['to-do', 'in-progress', 'done'];
+
+  const [isClicked, setIsClicked] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('');
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+  async function handleOptionChange(event) {
+    await setSelectedOption(event.target.value);
+    editTask = { ...editTask, category: event.target.value };
+
+    dispatch(patchTask({ id: id, task: editTask }));
+    setIsClicked(false);
+  }
+
   return (
     <Wraper>
-      <div>
-        <fieldset>
-          {category.map(item => {
-            if (item !== editTask.category) {
-              return (
-                <div key={item._id}>
-                  <input
-                    type="radio"
-                    id={id}
-                    name={id}
-                    value={item}
-                    onChange={event => {
-                      console.log('меняем прогресс na', item);
-                      console.log('editTask', editTask);
-                      console.log('editTask.category:', editTask.category);
-                      console.log(typeof editTask);
-                      console.log(typeof item);
-
-                      editTask = { ...editTask, category: item };
-                      console.log('editTask new', editTask);
-                      dispatch(patchTask({ id: id, task: editTask }));
-                    }}
-                  />
-                  <label htmlFor={id}>{item}</label>
-                </div>
-              );
-            }
-            return null;
-          })}
-        </fieldset>
-        {/* 
-        <select class="ice-cream" name="ice-cream">
-          <option value="">
-            <ToolBarItem>
-              <use href={`${Icons}#task-move-sf`}></use>
-            </ToolBarItem>
-          </option>
-          {category.map(item => {
-            if (item !== editTask.category) {
-              return <option value={item}>{item}</option>;
-            }
-          })}
-        </select> */}
-
+      <BtnArrow>
         <BtnStyled
           type="button"
           onClick={() => {
-            console.log('меняем прогресс');
-            // dispatch(patchTask(id, editTask));
+            handleClick();
           }}
         >
           <ToolBarItem>
             <use href={`${Icons}#task-move-sf`}></use>
           </ToolBarItem>
         </BtnStyled>
-      </div>
+        <ChouseCat
+          style={isClicked ? { display: 'flex' } : { display: 'none' }}
+        >
+          {category.map(item => {
+            return (
+              item !== editTask.category && (
+                <div key={Math.random()}>
+                  <LabelStyled>
+                    {item}
+                    <ToolBarItem>
+                      <use href={`${Icons}#task-move-sf`}></use>
+                    </ToolBarItem>
+                    <InputStyled
+                      type="radio"
+                      value={item}
+                      checked={selectedOption === { item }}
+                      onChange={handleOptionChange}
+                    />
+                  </LabelStyled>
+                </div>
+              )
+            );
+          })}
+        </ChouseCat>
+      </BtnArrow>
 
       <BtnStyled type="button" onClick={() => console.log('редактируем')}>
         <ToolBarItem>
@@ -77,7 +80,6 @@ const TaskToolBar = ({ id }) => {
       <BtnStyled
         type="button"
         onClick={() => {
-          console.log('id to dellete:', id);
           dispatch(deleteTask(id));
         }}
       >
