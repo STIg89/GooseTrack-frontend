5fed86c0c01eec63616814134465d3c796f15e65
Alona Panchenko <alona_panchenko@ukr.net>
2023-06-04 07:24:21 +0300

    add SlidersReview component

---
+++ src/components/Start/ReviewsSlider/Comment/Comment.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import sprite from '../../../../images/sprite.svg';
+
+import { CommentStyled,StarContainerStyled } from "./Comment.styled";
+
+export function Comment(props){
+    return(
+            <CommentStyled>
+            <div className="comment-container">
+            <div className="comment-box">
+                <img src={props.src} alt='User avatar' />
+            </div>
+            <div>
+                <h3>{props.name.trim()}</h3>
+                  <StarContainerStyled value={props.star}>
+                    <li><svg height="14" width="14">
+                      <use href={sprite+'#starStartPage'}></use>
+                    </svg></li>
+                      <li><svg height="14" width="14">
+                          <use href={sprite+'#starStartPage'}></use>
+                      </svg></li>
+                      <li><svg height="14" width="14">
+                          <use href={sprite+'#starStartPage'}></use>
+                      </svg></li>
+                      <li><svg height="14" width="14">
+                          <use href={sprite+'#starStartPage'}></use>
+                      </svg></li>
+                      <li><svg height="14" width="14">
+                          <use href={sprite+'#starStartPage'}></use>
+                      </svg></li>
+                  </StarContainerStyled>
+              </div>
+            </div>
+            <p className="comment-content">{props.children}</p>
+        </CommentStyled >
+    );
+}
--- src/components/Start/ReviewsSlider/ReviewsSlider.jsx
+++ src/components/Start/ReviewsSlider/ReviewsSlider.jsx
@@ -1,8 +1,65 @@
 import React from 'react';
-//import Slider from "react-slick";
+import Slider from "react-slick";
 
-export function ReviewsSlider(){
+import { Comment } from "./Comment/Comment";
+import { users } from "./usersDB"
+import { ReviewsSliderStyled } from "./ReviewsSlider.styled";
+import sprite from '../../../images/sprite.svg';
+
+const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
+  <div {  ...props} 
+        className={ "slick-prev slick-arrow" + (currentSlide === 0 ? " slick-disabled" : "") }
+        aria-hidden="true"
+        aria-disabled={false}
+        type="button">
+      <svg height="6" width="39" className="svg-arrow">
+          <use href={sprite+'#arRight'}></use>
+      </svg>
+  </div>
+);
+
+const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+  <div {...props}
+        className={ "slick-next slick-arrow" + (currentSlide === slideCount - 1 ? " slick-disabled" : "") }
+        aria-hidden="true"
+        aria-disabled={ false}
+        type="button">
+      <svg height="6" width="39" className="svg-arrow">
+          <use href={sprite+'#arLeft'}></use>
+      </svg>
+  </div>
+);
+
+
+export default function ReviewsSlider(){
+
+      const settings={
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow:2,
+        slidesToScroll: 1,
+        adaptiveHeight:true,
+        responsive:[
+            {
+                breakpoint:1439,
+                settings:{
+                    slidesToShow:1,
+                }
+            }
+        ],
+        nextArrow:<SlickArrowRight></SlickArrowRight>,
+        prevArrow:<SlickArrowLeft></SlickArrowLeft>
+      }
     
 return(
-    <ReviewsSlider />
-);}
+    <ReviewsSliderStyled>
+        <section className="content-review">
+            <h2 className="content-h2">Reviews</h2>
+            <Slider  className="slider"  {...settings} >
+                {users.map( ({ name, star, text, avatar }, index) =>
+                    <Comment key={index} className='tr' src={avatar} star={star} name={name}>{text}</Comment>)
+                }
+            </Slider>
+        </section>
+    </ReviewsSliderStyled>);}
