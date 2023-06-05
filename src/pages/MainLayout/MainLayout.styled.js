5d983e27a75fcaccb863cff33d40b2d24ac57a80
STIg89 <andreystig89@gmail.com>
2023-06-05 22:25:48 +0300

    Update MainLayout.styled.js

--- src/pages/MainLayout/MainLayout.styled.js
+++ src/pages/MainLayout/MainLayout.styled.js
@@ -1,24 +1,23 @@
 import styled from 'styled-components';
 
 const media = {
-  tablet: "@media screen and (min-width: 768px)",
-  desktop: "@media screen and (min-width: 1440px)"
+  tablet: '@media screen and (min-width: 768px)',
+  desktop: '@media screen and (min-width: 1440px)',
 };
 
 export const MainContainer = styled.div`
+  margin-top: 24px;
+  margin-left: 20px;
+  margin-right: 20px;
 
-margin-top: 24px; 
-margin-left:20px;
-margin-right:20px;
-
-${media.tablet} {
-  margin-top: 24px; 
-  margin-left:32px;
-  margin-right:32px;
-  } 
-
-${media.desktop} {
-  margin-top: 40px; 
+  ${media.tablet} {
+    margin-top: 24px;
+    margin-left: 32px;
+    margin-right: 32px;
   }
 
-`
+  ${media.desktop} {
+    margin-top: 40px;
+    margin-left: 320px;
+  }
+`;
