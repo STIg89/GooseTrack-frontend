bb82747f088ccf8aadc3153fab3709ef6bcb4e94
ViktorSolomianiy <xmegasystem35@gmail.com>
2023-06-07 16:44:58 +0300

    draft

--- src/components/Calendar/ChoosedDay/TasksColumnsList/TasksColumn/TaskModal/TaskModal.jsx
+++ src/components/Calendar/ChoosedDay/TasksColumnsList/TasksColumn/TaskModal/TaskModal.jsx
@@ -1,10 +1,15 @@
 import Modal from 'components/Modal/Modal';
 import TaskForm from './TaskForm/TaskForm';
 
-export const TaskModal = ({ onCloseModal }) => {
+const TaskModal = ({ onCloseModal, showEditBtn, id, category }) => {
   return (
     <Modal onCloseModal={onCloseModal}>
-      <TaskForm onCloseModal={onCloseModal} />
+      <TaskForm
+        onCloseModal={onCloseModal}
+        showEditBtn={showEditBtn}
+        id={id}
+        category={category}
+      />
     </Modal>
   );
 };
