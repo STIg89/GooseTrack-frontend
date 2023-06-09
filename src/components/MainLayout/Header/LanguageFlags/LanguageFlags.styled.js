5ee168632a351a34ab777483425889ba91ae7729
Iryna Zaitseva <zaitseva02@gmail.com>
2023-06-10 01:11:36 +0300

    flags

--- src/components/MainLayout/Header/LanguageFlags/LanguageFlags.styled.js
+++ src/components/MainLayout/Header/LanguageFlags/LanguageFlags.styled.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import LanguageIcon from '@mui/icons-material/Language';
 
 const media = {
-  // mobile: '@media screen and (max-width: 374px)',
   tablet: '@media screen and (min-width: 768px)',
   desktop: '@media screen and (min-width: 1440px)',
 };
