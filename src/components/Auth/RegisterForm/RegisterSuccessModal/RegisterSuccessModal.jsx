8fac880a1dc70e8b1ea041173eda693d2903976b
STIg89 <andreystig89@gmail.com>
2023-06-11 18:26:56 +0300

    Update RegisterSuccessModal.jsx

--- src/components/Auth/RegisterForm/RegisterSuccessModal/RegisterSuccessModal.jsx
+++ src/components/Auth/RegisterForm/RegisterSuccessModal/RegisterSuccessModal.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import Modal from 'components/Modal/Modal';
 
-const RegistrationSuccessModal = ({
-  onResendEmail,
-  onCloseModal,
-  isOpened,
-}) => {
-  console.log(onCloseModal);
-  console.log(isOpened);
+const RegistrationSuccessModal = ({ onResendEmail, onCloseModal }) => {
   return (
-    <Modal onCloseModal={onCloseModal} isOpened={isOpened}>
+    <Modal onCloseModal={onCloseModal}>
       <h2>Registration Successful</h2>
       <p>You are successfully registered! Verification sent to your e-mail</p>
       <button onClick={onResendEmail}>Resend Email</button>
