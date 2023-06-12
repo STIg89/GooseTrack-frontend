26e37eb2da8bb29428c9407d9dc9f47669a206f0
Halyna Kilbovych <108695210+HalynaKilbovych@users.noreply.github.com>
2023-06-12 12:38:28 +0300

    add resend-modal

--- src/components/Auth/RegisterForm/RegisterSuccessModal/RegisterSuccessModal.jsx
+++ src/components/Auth/RegisterForm/RegisterSuccessModal/RegisterSuccessModal.jsx
@@ -1,13 +1,58 @@
-import React from 'react';
-import Modal from 'components/Modal/Modal';
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { Overlay, CloseIcon } from 'components/Modal/Modal.styled';
+import Icons from 'images/sprite.svg';
+import { ResendEmailWrapper, Title, Text, ResendButton } from './RegisterSuccessModal.styled';
 
-const RegistrationSuccessModal = ({ onResendEmail, onCloseModal }) => {
+const modalRoot = document.getElementById('modal-root');
+
+const RegistrationModal = ({ children, onCloseModal}) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    const onKeyDown = e => {
+      if (e.code === 'Escape') {
+        onCloseModal();
+      }
+    };
+
+    window.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+      document.body.style.overflow = 'auto';
+    };
+  }, [onCloseModal]);
+
+  const handleOverlayClick = ({ currentTarget, target }) => {
+    if (currentTarget !== target) {
+      return;
+    }
+    onCloseModal();
+  };
+
+  return createPortal(
+    <Overlay
+      onClick={handleOverlayClick}
+    >
+      <ResendEmailWrapper>
+        <CloseIcon onClick={() => onCloseModal()}>
+          <use href={`${Icons}#icon-close`}></use>
+        </CloseIcon>
+        {children}
+      </ResendEmailWrapper>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+const RegistrationSuccessModal = ({ onResendEmail, onCloseModal, email }) => {
   return (
-    <Modal onCloseModal={onCloseModal}>
-      <h2>Registration Successful</h2>
-      <p>You are successfully registered! Verification sent to your e-mail</p>
-      <button onClick={onResendEmail}>Resend Email</button>
-    </Modal>
+    <RegistrationModal onCloseModal={onCloseModal}>
+      <Title>Registration Successful</Title>
+      <Text>You are successfully registered! Verification sent to your e-mail</Text>
+      <ResendButton onClick={() => onResendEmail(email)}>Resend Email</ResendButton>
+    </RegistrationModal>
   );
 };
 
