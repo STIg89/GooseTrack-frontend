7128ca2c8e5d8c467b8098d1c5ffb47f7c533c38
Halyna Kilbovych <108695210+HalynaKilbovych@users.noreply.github.com>
2023-06-15 15:48:28 +0300

    fix arrow remove scroll

--- src/components/Start/ReviewsSlider/Comment/Comment.jsx
+++ src/components/Start/ReviewsSlider/Comment/Comment.jsx
@@ -1,48 +1,62 @@
 import React from "react";
 import sprite from '../../../../images/sprite.svg';
 
-import { CommentStyled,StarContainerStyled } from "./Comment.styled";
+import { CommentStyled, StarContainerStyled } from "./Comment.styled";
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 
 export function Comment(props){
-    const scrollType = props.children.length>200 ? "auto" : "visible"
+    const scrollType = "visible";
+    const content = props.children.length > 200 ? `${props.children.slice(0, 200)}...` : props.children;
+    const truncatedContent = props.children.length > 200 ? `${content}...` : content;
     const LetterAvatar = props.name.indexOf(' ') !== -1 
         ?` ${props.name[0].toUpperCase()} ${props.name.split(' ').pop()[0].toUpperCase()} `
         : ` ${props.name[0].toUpperCase()} `;
     
-    return(
-            <CommentStyled>
+    return (
+        <CommentStyled>
             <div className="comment-container">
-            <div className="comment-box">
-                {props.src  ? <img  src={props.src} alt={LetterAvatar}/>
-                            : <Stack>
-                                <Avatar sx={{ bgcolor:'#3e85f3'}}> {LetterAvatar} </Avatar>
-                              </Stack>
-                }
+                <div className="comment-box">
+                    {props.src ? (
+                        <img src={props.src} alt={LetterAvatar}/>
+                    ) : (
+                        <Stack>
+                            <Avatar sx={{ bgcolor:'#3e85f3'}}> {LetterAvatar} </Avatar>
+                        </Stack>
+                    )}
+                </div>
+                <div>
+                    <h3>{props.name.trim()}</h3>
+                    <StarContainerStyled value={props.star}>
+                        <li>
+                            <svg height="14" width="14">
+                                <use href={sprite+'#starStartPage'}></use>
+                            </svg>
+                        </li>
+                        <li>
+                            <svg height="14" width="14">
+                                <use href={sprite+'#starStartPage'}></use>
+                            </svg>
+                        </li>
+                        <li>
+                            <svg height="14" width="14">
+                                <use href={sprite+'#starStartPage'}></use>
+                            </svg>
+                        </li>
+                        <li>
+                            <svg height="14" width="14">
+                                <use href={sprite+'#starStartPage'}></use>
+                            </svg>
+                        </li>
+                        <li>
+                            <svg height="14" width="14">
+                                <use href={sprite+'#starStartPage'}></use>
+                            </svg>
+                        </li>
+                    </StarContainerStyled>
+                </div>
             </div>
-            <div>
-                <h3>{props.name.trim()}</h3>
-                  <StarContainerStyled value={props.star}>
-                    <li><svg height="14" width="14">
-                      <use href={sprite+'#starStartPage'}></use>
-                    </svg></li>
-                      <li><svg height="14" width="14">
-                          <use href={sprite+'#starStartPage'}></use>
-                      </svg></li>
-                      <li><svg height="14" width="14">
-                          <use href={sprite+'#starStartPage'}></use>
-                      </svg></li>
-                      <li><svg height="14" width="14">
-                          <use href={sprite+'#starStartPage'}></use>
-                      </svg></li>
-                      <li><svg height="14" width="14">
-                          <use href={sprite+'#starStartPage'}></use>
-                      </svg></li>
-                  </StarContainerStyled>
-              </div>
-            </div>
-            <p className="comment-content"  style={{  overflow:scrollType  } } > {props.children}</p>
-        </CommentStyled >
+            <p className="comment-content" style={{ overflow: scrollType }}>{truncatedContent}</p>
+        </CommentStyled>
     );
 }
